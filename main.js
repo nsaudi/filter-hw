dbf3e831ea30648f263567505fa939d030fbeaf0
Dr Moe <snannann@Moes-MacBook-Air.local>
2017-04-28 17:15:46 -0400

    adding files

--- main.js
+++ main.js
@@ -5,11 +5,30 @@ $(document).ready(function () {
 		var button = $(this);
 		var filter = button.data("filter");
 
+		button.toggleClass("selected");
+
 		if (filter === "all") {
-			$(".item").addClass("active");
+			$(".selected").removeClass("active");
+			$(".show").addClass("active");
+
+			// $(".show.active").removeClass("active");
+
 		} else {
-			$(".item.active").removeClass("active");
-			$("." + filter).addClass("active");
+			$(".show.active").addClass("active");
+			// $("." + filter).addClass("active");
+
+			$(".filter.selected").each(function () {
+				var button = $(this);
+				var filter = button.data("filter");
+				$("." + filter).addClass("active");
+			});
+
+			// $(".everything").on("click"function (e) {
+			// 	e.preventDefault();
+			// 	$(".filter.selected").removeClass("selected");
+			// 	$("." + filter).addClass("active");
+			// });
 		}
+
 	});
 });
